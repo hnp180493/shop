2ce58f67cb7a8879afeedd2305e6f37e20ce9109
hnp180493 <hnp180493@gmail.com>
2018-11-25 10:53:08 +0700

    Bai_6

---
+++ Shop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,10 @@
+﻿using Shop.Data;
+using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        ShopDbContext Init();
+    }
+}
---
+++ Shop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private ShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public ShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
