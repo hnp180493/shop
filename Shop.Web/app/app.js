10d08e784b0366e9c7ce1068e3da8b4fc644e9bd
hnp180493 <hnp180493@gmail.com>
2018-12-18 19:19:48 +0700

    remove authen

--- Shop.Web/app/app.js
+++ Shop.Web/app/app.js
@@ -10,23 +10,18 @@
 
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('base', {
-                url: '',
-                templateUrl: '/app/shared/views/baseView.html',
-                abstract: true
-            }).state('login', {
+            .state('login', {
                 url: "/login",
                 templateUrl: "/app/components/login/loginView.html",
                 controller: "loginController"
             })
-            .state('home', {
+            .state('admin', {
                 url: "/admin",
-                parent: 'base',
                 templateUrl: "/app/components/home/homeView.html",
                 controller: "homeController"
             });
 
-        $urlRouterProvider.otherwise("/login");
+        $urlRouterProvider.otherwise("/admin");
     }
 
     function configAuthentication($httpProvider) {
--- Shop.Web/app/components/products/products.module.js
+++ Shop.Web/app/components/products/products.module.js
@@ -5,17 +5,14 @@
         $stateProvider.state('products', {
             url: "/products",
             templateUrl: "/app/components/products/productListView.html",
-            parent: 'base',
             controller: "productListController"
         }).state('add_product', {
             url: "/add_product",
             templateUrl: "/app/components/products/productAddView.html",
-            parent: 'base',
             controller: "productAddController"
         }).state('edit_product', {
             url: "/edit_product/:id",
             templateUrl: "/app/components/products/productEditView.html",
-            parent: 'base',
             controller: "productEditController"
         });
     }
