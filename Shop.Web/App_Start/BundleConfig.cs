7c430a35796771d7899ad127773281fa245f7688
hnp180493 <hnp180493@gmail.com>
2018-12-18 15:22:45 +0700

    12

--- Shop.Web/App_Start/BundleConfig.cs
+++ Shop.Web/App_Start/BundleConfig.cs
@@ -2,7 +2,7 @@
 using System.Web.Optimization;
 
 namespace Shop.Web
-{1
+{
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
